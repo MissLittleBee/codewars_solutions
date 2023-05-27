fde5cd81936d995edfb0738781c24b64a3e86987
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2023-05-27 21:27:36 +0200

    added 1 new katas

---
+++ output/isograms.py
@@ -0,0 +1,29 @@
+"""Kata - Isograms
+
+completed at: 2023-05-27 19:25:30
+by: Jakub Červinka
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+**Example: (Input --> Output)**
+```if-not:factor
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+```
+
+```if:factor
+"Dermatoglyphics" -> t
+"aba" -> f
+"moOse" -> f (ignore letter case)
+```
+```fsharp
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+```
+
+"""
+
+def is_isogram(string):
+    return len(set(string.lower())) == len(string.lower())
