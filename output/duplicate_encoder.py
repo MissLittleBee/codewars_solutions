67279fe500e3f77fd7db7129b357fa188262772f
Jakub Cervinka PLN DIWTZ13 <jakub.cervinka@zf.com>
2023-05-30 09:06:39 +0200

    added 249 new katas

--- output/duplicate_encoder.py
+++ output/duplicate_encoder.py
@@ -1,6 +1,6 @@
 """Kata - Duplicate Encoder
 
-completed at: 2019-05-06 08:35:26
+completed at: 2019-05-06 08:37:49
 by: Jakub Červinka
 
 The goal of this exercise is to convert a string to a new string where each character in the new string is `"("` if that character appears only once in the original string, or `")"` if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
