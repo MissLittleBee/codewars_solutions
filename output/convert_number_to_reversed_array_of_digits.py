ae8bc3e95b8739acc522d8971e8aa851921992d8
JakubDotPy <JakubDotPy@users.noreply.github.com>
2024-04-03 17:34:37 +0000

    Apply automatic changes

---
+++ output/convert_number_to_reversed_array_of_digits.py
@@ -0,0 +1,19 @@
+"""Kata - Convert number to reversed array of digits
+
+completed at: 2024-03-23 13:00:20
+by: Jakub Červinka
+
+# Convert number to reversed array of digits
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+## Example(Input => Output):
+
+```
+35231 => [1,3,2,5,3]
+0 => [0]
+```
+
+"""
+
+None
---
+++ output/fundamentals_return.py
@@ -0,0 +1,46 @@
+"""Kata - Fundamentals: Return
+
+completed at: 2024-03-12 14:57:16
+by: Jakub Červinka
+
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+```if-not:csharp
+addition = **add**
+
+multiply = **multiply**
+
+division = **divide** (both integer and float divisions are accepted)
+
+modulus = **mod**
+
+exponential = **exponent**
+
+subtraction = **subt**
+```
+
+```if:csharp
+addition = **Add**
+
+multiply = **Multiply**
+
+division = **Divide** 
+
+modulus = **Mod**
+
+exponential = **Exponent**
+
+subtraction = **Subt**
+
+**Note: All funcitons can return int and all will recieve 2 integers.** 
+```
+
+
+*Note: All math operations will be:
+  a (operation) b*
+
+"""
+
+None
