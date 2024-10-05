c9263587d8dcc39c8583684a745319ddf1a53a9c
MissLittleBee <MissLittleBee@users.noreply.github.com>
2024-10-05 01:04:12 +0000

    Automated update

---
+++ output/abbreviate_a_two_word_name.py
@@ -0,0 +1,27 @@
+"""Kata - Abbreviate a Two Word Name
+
+completed at: 2024-10-04 20:54:06
+by: Barbora Hůlová
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+`Sam Harris` => `S.H`
+
+`patrick feeney` => `P.F`
+
+~~~if:riscv
+RISC-V: The function signature is:
+
+```c
+char *get_initials(const char *full_name, char initials[4]);
+```
+
+Write your result to `initials`, and return that buffer.
+~~~
+"""
+
+None
---
+++ output/total_amount_of_points.py
@@ -0,0 +1,26 @@
+"""Kata - Total amount of points
+
+completed at: 2024-10-04 21:33:14
+by: Barbora Hůlová
+
+Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format `"x:y"`, where `x` is our team's score and `y` is our opponents score.
+
+For example:
+```["3:1", "2:2", "0:1", ...]```
+
+Points are awarded for each match as follows:
+- if x > y: 3 points (win)
+- if x < y: 0 points (loss)
+- if x = y: 1 point  (tie)
+
+We need to write a function that takes this collection and returns the number of points our team (`x`) got in the championship by the rules given above.
+
+Notes:
+- our team always plays 10 matches in the championship
+- 0 <= x <= 4
+- 0 <= y <= 4
+"""
+
+None
