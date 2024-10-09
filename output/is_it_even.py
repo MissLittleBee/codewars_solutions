902599a5882838fb1c2429ee4cbde385e5af9ecd
MissLittleBee <MissLittleBee@users.noreply.github.com>
2024-10-09 01:04:38 +0000

    Automated update

---
+++ output/is_it_even.py
@@ -0,0 +1,17 @@
+"""Kata - Is it even?
+
+completed at: 2024-10-08 17:08:36
+by: Barbora Hůlová
+
+In this Kata we are passing a number (n) into a function. 
+
+Your code will determine if the number passed is even (or not). 
+
+The function needs to return either a true or false. 
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+"""
+
+None
