fe4cc2fac9c417d4a205768d1664e38a7a3d2517
MissLittleBee <MissLittleBee@users.noreply.github.com>
2025-05-13 01:22:20 +0000

    Automated update

---
+++ output/array_plus_array.py
@@ -0,0 +1,11 @@
+"""Kata - Array plus array
+
+completed at: 2025-05-12 18:15:51
+by: Barbora Hůlová
+
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+"""
+
+None
---
+++ output/fix_your_code_before_the_garden_dies.py
@@ -0,0 +1,12 @@
+"""Kata - Fix your code before the garden dies! 
+
+completed at: 2025-05-12 18:31:44
+by: Barbora Hůlová
+
+You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs. 
+
+Your task is to debug the code before your plants die!
+
+"""
+
+None
