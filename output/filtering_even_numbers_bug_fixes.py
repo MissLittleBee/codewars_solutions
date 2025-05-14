43048263e30764630a4e02cfd5ad51907390b2fe
MissLittleBee <MissLittleBee@users.noreply.github.com>
2025-05-14 01:20:57 +0000

    Automated update

---
+++ output/filtering_even_numbers_bug_fixes.py
@@ -0,0 +1,11 @@
+"""Kata - Filtering even numbers (Bug Fixes)
+
+completed at: 2025-05-13 19:29:32
+by: Barbora Hůlová
+
+The method is supposed to remove even numbers from the list and return a list that contains the odd numbers.
+
+However, there is a bug in the method that needs to be resolved.
+"""
+
+None
